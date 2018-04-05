853fdca125d43bc9fe8063ac903c395159c32232
Joey <joeybailey284@gmail.com>
2018-04-05 11:57:42 -0400

    Added Name

---
+++ Game.py
@@ -0,0 +1,4 @@
+playerName = input("whats your name? > ")
+print(playerName)
+instrument = input("what instrument do you play? > ")
+print ("your name is {}. you play {}".format(playerName, instrument))
---
+++ readme.md
@@ -0,0 +1,1 @@
+this will be a text based adventure game about band camp. there will be Drama
